5f1c4a38a545b0233f80ca6078bf0b9c3f2f04a0
Rodion Grigiola <ronnyespada@gmail.com>
2022-02-26 17:59:22 +0100

    //assignment_1

--- lab2.php
+++ lab2.php
@@ -12,4 +12,15 @@ $order .= " random string";
 //// Don't change the line below
 echo "Your order is: $very_bad_unclear_name.\n";
 
+//assignment_2
 
+$var_1 = 5;
+echo $var_1 . "\n";
+$var_2 = 10;
+echo $var_2;
+$var_3 = 15.5;
+echo (11+1) . "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo ($last_month - $this_month);
